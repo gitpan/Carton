56742dbd01f2e3ea0b50749511d53e9cf0b466df
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-08-11 04:55:39 +0000

    Import of MIYAGAWA/Carton-v1.0.7 from CPAN.
    gitpan-cpan-distribution: Carton
    gitpan-cpan-version:      v1.0.7
    gitpan-cpan-path:         MIYAGAWA/Carton-v1.0.7.tar.gz
    gitpan-cpan-author:       MIYAGAWA
    gitpan-cpan-maturity:     released

--- lib/Carton/Packer.pm
+++ lib/Carton/Packer.pm
@@ -3,6 +3,8 @@ use strict;
 use App::FatPacker;
 use File::pushd ();
 use Path::Tiny ();
+use CPAN::Meta ();
+use File::Find ();
 
 use Moo;
 
@@ -23,10 +25,22 @@ $Carton::Fatpacked = 1;
 exit Carton::CLI->new->run(@ARGV);
 EOF
 
+    my $fatpacked = $self->do_fatpack($file);
+
+    my $executable = $dir->child('carton');
+    warn "Bundling $executable\n";
+
+    $dir->mkpath;
+    $executable->spew($fatpacked);
+    chmod 0755, $executable;
+}
+
+sub do_fatpack {
+    my($self, $file) = @_;
+
     my $packer = App::FatPacker->new;
 
-    my @modules = split /\r?\n/, $packer->trace(args => [$file], use => ['App::cpanminus']);
-
+    my @modules = split /\r?\n/, $packer->trace(args => [$file], use => $self->required_modules);
     my @packlists = $packer->packlists_containing(\@modules);
     $packer->packlists_to_tree(Path::Tiny->new('fatlib')->absolute, \@packlists);
 
@@ -35,12 +49,48 @@ EOF
         $packer->fatpack_file($file);
     };
 
-    my $executable = $dir->child('carton');
-    warn "Bundling $executable\n";
+    # HACK: File::Spec bundled into arch in < 5.16, but is loadable as pure-perl
+    use Config;
+    $fatpacked =~ s/\$fatpacked{"$Config{archname}\/(Cwd|File)/\$fatpacked{"$1/g;
 
-    $dir->mkpath;
-    $executable->spew($fatpacked);
-    chmod 0755, $executable;
+    $fatpacked;
+}
+
+sub required_modules {
+    my($self, $packer) = @_;
+
+    my $meta = $self->installed_meta('Carton')
+        or die "Couldn't find install metadata for Carton";
+
+    my %excludes = (
+        perl => 1,
+        'ExtUtils::MakeMaker' => 1,
+        'Module::Build' => 1,
+    );
+
+    my @requirements = grep !$excludes{$_},
+        $meta->effective_prereqs->requirements_for('runtime', 'requires')->required_modules;
+
+    return \@requirements;
+}
+
+sub installed_meta {
+    my($self, $dist) = @_;
+
+    my @meta;
+    my $finder = sub {
+        if (m!\b$dist-.*[\\/]MYMETA.json!) {
+            my $meta = CPAN::Meta->load_file($_);
+            push @meta, $meta if $meta->name eq $dist;
+        }
+    };
+
+    File::Find::find({ wanted => $finder, no_chdir => 1 }, grep -d, map "$_/.meta", @INC);
+
+    # return the latest version
+    @meta = sort { version->new($b->version) cmp version->new($a->version) } @meta;
+
+    return $meta[0];
 }
 
 1;
